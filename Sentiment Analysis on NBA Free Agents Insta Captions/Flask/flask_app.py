d95f8af3867b35d33b765686d4f472c2ae7d9619
Cyrus Rustomji <cyrusrustomji@gmail.com>
2018-05-31 19:00:38 -0700

    flask update

---
+++ Sentiment Analysis on NBA Free Agents Insta Captions/Flask/flask_app.py
@@ -0,0 +1,36 @@
+import flask
+
+#---------- URLS AND WEB PAGES -------------#
+
+# Initialize the app
+app = flask.Flask(__name__)
+
+# Homepage
+@app.route("/")
+def viz_page():
+    """
+    Homepage: serve our visualization page, awesome.html
+    """
+    with open("index.html", 'r') as viz_file:
+        return viz_file.read()
+
+@app.route("/score", methods=["POST"])
+def score():
+    """
+    When A POST request with json data is made to this uri,
+    Read the example from the json, predict probability and
+    send it with a response
+    """
+    # Get decision score for our example that came with the request
+    data = flask.request.json
+    print("-------------%s---------"% data)
+    # Put the result in a nice dict so we can send it as json
+    results = {"score": score}
+    return flask.jsonify(results)
+
+#--------- RUN WEB APP SERVER ------------#
+
+# Start the app server on port 80
+# (The default website port)
+app.run(host='0.0.0.0')
+app.run(debug=True)
